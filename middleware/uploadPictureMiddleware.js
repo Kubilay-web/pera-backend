4814caae4db8ddaf76bfd530ff81b6f7ff14f86b
Kubilay Özdemir <kubilayozdemir95@gmail.com>
2024-10-23 11:44:01 +0300

    Upload

--- middleware/uploadPictureMiddleware.js
+++ middleware/uploadPictureMiddleware.js
@@ -1,6 +1,5 @@
 // const multer = require("multer");
 // const path = require("path");
-
 // const storage = multer.diskStorage({
 //   destination: (req, file, cb) => {
 //     cb(null, path.join(__dirname, "../uploads"));
@@ -9,7 +8,6 @@
 //     cb(null, `${Date.now()}-${file.originalname}`);
 //   },
 // });
-
 // const uploadPicture = multer({
 //   storage: storage,
 //   limits: {
@@ -23,32 +21,34 @@
 //     cb(null, true);
 //   },
 // });
-
 // module.exports = { uploadPicture };
 
-////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
 
 const multer = require("multer");
-const cloudinary = require("cloudinary").v2;
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const path = require("path");
 
-// Cloudinary yapılandırması
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-// Cloudinary storage ayarları
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: "blog-app", // Cloudinary'deki klasör adı
-    allowed_formats: ["jpg", "jpeg", "png"],
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../uploads"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
   },
 });
 
-// multer'ı CloudinaryStorage ile kullan
-const uploadPicture = multer({ storage: storage });
+const uploadPicture = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1 * 1000000, // 1 MB
+  },
+  fileFilter: function (req, file, cb) {
+    let ext = path.extname(file.originalname);
+    if (ext !== ".png" && ext !== ".jpg" && ext !== ".jpeg") {
+      return cb(new Error("Only images are allowed"));
+    }
+    cb(null, true);
+  },
+});
 
 module.exports = { uploadPicture };
